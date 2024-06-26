a2429b89661d212d29e73212c23fffdff53725af
Bhargav Mule <bhargavmule16@gmail.com>
2024-06-26 10:22:29 +0530

    fixes

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ const server = http.createServer(app)
 
 const io = new Server(server , {
     cors: {
-        origin: process.env.CLIENT_URL,
+        origin: 'https://creamynuts.oneminus.in',
         credentials: true
     }
 })
